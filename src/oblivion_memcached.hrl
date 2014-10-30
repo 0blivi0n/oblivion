693ff06f77fa0792a729b85fe3174ae4ee2084ca
goncalo-pereira <goncalo.pereira@caixadecorreio.eu>
2014-10-30 10:25:05 +0000

    Added copyright information

--- src/oblivion_memcached.hrl
+++ src/oblivion_memcached.hrl
@@ -1,3 +1,19 @@
+%%
+%% Copyright 2014 Joaquim Rocha <jrocha@gmailbox.org>
+%% 
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+
 -define(HEADER_SIZE_REQUEST, 24).
 -define(DEFAULT_FLAG_VALUE, 16#deadbeef). %% TODO Nao encontrei informacao sobre a flag
 
