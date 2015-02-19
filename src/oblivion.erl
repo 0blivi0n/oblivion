0d6a062f5f70cf4e6e7b8e1f588a0a72237b1da1
Joaquim Rocha <jrocha@gmailbox.org>
2015-02-19 01:47:39 +0000

    Fix #5

--- src/oblivion.erl
+++ src/oblivion.erl
@@ -21,6 +21,17 @@
 
 -define(SERVER, {local, ?MODULE}).
 
+-define(SYNC_TIMEOUT, 5000).
+
+-define(CACHE_CONFIG(Name, Options), {Name, Options}).
+
+-define(NODES_CONFIG_PARAM, node_list).
+-define(CACHE_CONFIG_PARAM, cache_list).
+-define(CONFIG_DATA(NodeConfig, CacheConfig), [
+		{?NODES_CONFIG_PARAM, NodeConfig}, 
+		{?CACHE_CONFIG_PARAM, CacheConfig}
+		]).
+
 %% ====================================================================
 %% API functions
 %% ====================================================================
@@ -83,29 +94,42 @@ delete_cache(CacheName) ->
 %% Behavioural functions
 %% ====================================================================
 
--record(state, {nodes=[]}).
+-record(state, {nodes, caches, persistence_file}).
 
 %% init
 init([]) ->
 	process_flag(trap_exit, true),
-	error_logger:info_msg("~p starting on [~p]...\n", [?MODULE, self()]),
-	{ok, #state{}}.
+	case load_persistence() of
+		{error, Reason} -> 
+			error_logger:error_msg("~p fail to start because [~p]\n", [?MODULE, Reason]),
+			{stop,Reason};
+		{ok, PersistenceFile, Nodes, Caches} -> 
+			error_logger:info_msg("~p starting on [~p]...\n", [?MODULE, self()]),
+			{ok, #state{nodes=Nodes, caches=Caches, persistence_file=PersistenceFile}}
+	end.
 
 %% handle_call
-handle_call({create_cache, CacheName, Options}, _From, State=#state{nodes=Nodes}) ->
-	Reply = create_cache(CacheName, Options, Nodes),
-	notify({create_cache, CacheName, Options}, Nodes),
-	{reply, Reply, State};
+handle_call({create_cache, CacheName, Options}, _From, State=#state{nodes=Nodes, caches=Caches, persistence_file=FileName}) ->
+	case create_cache(CacheName, Options, Nodes) of
+		ok -> 
+			notify({create_cache, CacheName, Options}, Nodes),
+			Caches1 = lists:keystore(CacheName, 1, Caches, {CacheName, Options}),
+			store_config(FileName, ?CONFIG_DATA(Nodes, Caches1)),
+			{reply, ok, State#state{caches=Caches1}};
+		Other -> {reply, Other, State}
+	end;
 
-handle_call({delete_cache, CacheName}, _From, State=#state{nodes=Nodes}) ->
+handle_call({delete_cache, CacheName}, _From, State=#state{nodes=Nodes, caches=Caches, persistence_file=FileName}) ->
 	gibreel:delete_cache(CacheName),
 	notify({delete_cache, CacheName}, Nodes),
-	{reply, ok, State};
+	Caches1 = lists:keydelete(CacheName, 1, Caches),
+	store_config(FileName, ?CONFIG_DATA(Nodes, Caches1)),
+	{reply, ok, State#state{caches=Caches1}};
 
 handle_call({get_node_list}, _From, State=#state{nodes=Nodes}) ->
 	{reply, Nodes, State};
 
-handle_call({add_node, Node}, _From, State=#state{nodes=Nodes}) ->
+handle_call({add_node, Node}, _From, State=#state{nodes=Nodes, caches=Caches, persistence_file=FileName}) ->
 	NewNodes = case lists:member(Node, Nodes) of
 		true -> Nodes;
 		false -> 
@@ -114,16 +138,18 @@ handle_call({add_node, Node}, _From, State=#state{nodes=Nodes}) ->
 			columbo:refresh(),
 			update_caches(Nodes1),
 			notify({nodes, [node()|Nodes1]}, Nodes1),
+			store_config(FileName, ?CONFIG_DATA(Nodes1, Caches)),
 			Nodes1
 	end, 
 	{reply, ok, State#state{nodes=NewNodes}};
 
-handle_call({delete_node, Node}, _From, State=#state{nodes=Nodes}) ->
+handle_call({delete_node, Node}, _From, State=#state{nodes=Nodes, caches=Caches, persistence_file=FileName}) ->
 	NewNodes = case lists:member(Node, Nodes) of
 		true ->
 			Nodes1 = lists:delete(Node, Nodes),
 			update_caches(Nodes1),
 			notify({nodes, [node()|Nodes1]}, Nodes),
+			store_config(FileName, ?CONFIG_DATA(Nodes1, Caches)),
 			Nodes1;
 		false -> Nodes
 	end, 
@@ -133,18 +159,33 @@ handle_call({delete_node, Node}, _From, State=#state{nodes=Nodes}) ->
 handle_cast(_Msg, State) -> {noreply, State}.
 
 %% handle_info
-handle_info({create_cache, CacheName, Options}, State=#state{nodes=Nodes}) -> 
+handle_info({create_cache, CacheName, Options}, State=#state{nodes=Nodes, caches=Caches, persistence_file=FileName}) -> 
 	create_cache(CacheName, Options, Nodes),
-	{noreply, State};
+	Caches1 = lists:keystore(CacheName, 1, Caches, {CacheName, Options}),
+	store_config(FileName, ?CONFIG_DATA(Nodes, Caches1)),
+	{noreply, State#state{caches=Caches1}};
 
-handle_info({delete_cache, CacheName}, State) -> 
+handle_info({delete_cache, CacheName}, State=#state{nodes=Nodes, caches=Caches, persistence_file=FileName}) -> 
 	gibreel:delete_cache(CacheName),
-	{noreply, State};
+	Caches1 = lists:keydelete(CacheName, 1, Caches),
+	store_config(FileName, ?CONFIG_DATA(Nodes, Caches1)),
+	{noreply, State#state{caches=Caches1}};
 
-handle_info({nodes, NewNodes}, State) -> 
+handle_info({nodes, NewNodes}, State=#state{caches=Caches, persistence_file=FileName}) -> 
 	Nodes = lists:delete(node(), NewNodes),
 	update_caches(Nodes),
-	{noreply, State#state{nodes=Nodes}}.
+	store_config(FileName, ?CONFIG_DATA(Nodes, Caches)),
+	{noreply, State#state{nodes=Nodes}};
+
+handle_info({config_request, Cluster, From}, State=#state{nodes=Nodes, caches=Caches}) -> 
+	case get_cluster_name() of
+		Cluster -> From ! {config_response, ?CONFIG_DATA(Nodes, Caches)};
+		_ -> ok
+	end,
+	{noreply, State};
+
+handle_info(_Info, State) -> 
+	{noreply, State}.
 
 %% terminate
 terminate(_Reason, _State) -> ok.
@@ -182,3 +223,45 @@ create_cache(CacheName, Options, Nodes) ->
 notify(_Msg, []) -> ok;
 notify(Msg, Nodes) ->
 	spawn(fun() -> columbo:send_to_nodes(?MODULE, Nodes, Msg) end).
+
+load_persistence() ->
+	{ok, FileName} = application:get_env(oblivion, oblivion_persistence_file),
+	case file_exists(FileName) of
+		true ->
+			case read_config(FileName) of
+				{ok, SavedConfig} ->
+					?CONFIG_DATA(SavedNodes, _) = SavedConfig,
+					columbo:add_nodes(SavedNodes),
+					columbo:refresh(),
+					notify({config_request, get_cluster_name(), self()}, SavedNodes),
+					CurrentConfig = receive
+						{config_response, RemoteConfig} ->
+							store_config(FileName, RemoteConfig),
+							RemoteConfig
+					after ?SYNC_TIMEOUT -> SavedConfig
+					end,
+					?CONFIG_DATA(NewNodes, Caches) = CurrentConfig,
+					Nodes = lists:delete(node(), NewNodes),
+					lists:foreach(fun({CacheName, Options}) ->
+								create_cache(CacheName, Options, Nodes)
+						end, Caches),
+					{ok, FileName, Nodes, Caches};
+				{error, Reason} -> {error, Reason}
+			end;
+		false -> {ok, FileName, [], []}
+	end.
+
+file_exists(FileName) ->
+	case file:read_file_info(FileName) of
+		{ok, _} -> true;
+		{error, enoent} -> false;
+		{error, Reason} -> {error, Reason}
+	end.
+
+read_config(FileName) ->
+	file:script(FileName).
+
+store_config(FileName, Config) ->
+	{ok, H} = file:open(FileName, write),
+	io:format(H, "~p.", [Config]),
+	file:close(H).
